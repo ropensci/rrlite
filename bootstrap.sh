84956baa3761e9b5789eac5a0e0885d90c34cc6e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-03-08 10:19:02 +1100

    Add licence info for rlite

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-if [ ! -d src/rlite ]; then
-    git clone https://github.com/seppo0010/rlite src/rlite
+RLITE=src/rlite
+
+if [ ! -d $RLITE ]; then
+    echo "Cloning rlite:"
+    git clone https://github.com/seppo0010/rlite $RLITE
 fi
+
+RLITE_COPYING=inst/rlite.COPYING
+
+mkdir -p $(dirname $RLITE_COPYING)
+echo \
+"The following license applies to code from the rlite library which\n"\
+"will be linked into the installed package:\n" > inst/rlite.COPYING
+cat $RLITE/LICENSE >> $RLITE_COPYING
